1d9e342408d0833e0440d2308332e55a0f7db60b
joaoBrRj91 <joao1991algoritmo@gmail.com>
2023-11-12 13:23:53 -0300

    CQRS - Setup inicial na camada de Appplication do bounded context de vendas. - Criação do primeiro command adicionarItemPedidoCommand

---
+++ src/NewNerdStore.Core/Events/Messages/Command.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewNerdStore.Core.Events.Messages
+{
+    public abstract class Command : Message, IRequest<bool>
+    {
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; set; }
+
+        public ValidationResult ValidationResult { get; set; }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
